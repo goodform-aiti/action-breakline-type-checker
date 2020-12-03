779e4087f4c5519c26fe7b605969898c45d10a07
Amir Alian <amircup2006@gmail.com>
2020-12-03 11:56:17 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,17 +11,16 @@ printf ${MODIFIED_FILES}
 printf "\n*****************************\n"
 
 PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
-CSV_FILES=$(grep -P '.+\.csv$' <<< $PATHS)
 
 
-echo "$CSV_FILES" | while read FILE ; do
+echo "$PATHS" | while read FILE ; do
     if [[ ! -f $FILE ]]
     then
       # skip deleted files
       continue
     fi
-    CRLF_COUNT=$(grep -U $'\015' /var/www/html/includes/text | wc -l)
-    CR_COUNT=$(grep -U $'\x0D' /var/www/html/includes/text | wc -l)
+    CRLF_COUNT=$(grep -U $'\015' $FILE | wc -l)
+    CR_COUNT=$(grep -U $'\x0D' $FILE | wc -l)
     if [[ $CRLF_COUNT > 0 ]]
     then
       ERROR=101
