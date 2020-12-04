7dd99d16abe97c672f2944cbe20bbf2699c28e58
Amir Alian <amircup2006@gmail.com>
2020-12-04 12:22:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,9 +26,7 @@ echo "$PATHS" | while read FILE ; do
       continue
     fi
     
-    echo "++++++++++++++++"
     echo $(find $FILE -not -type d  -exec file "{}" ";")
-    echo "+++++++++++++++"
     
     CRLF_COUNT=$(find $FILE -not -type d  -exec file "{}" ";" | grep " CRLF " | cut -d " " -f 1 | cut -d ":" -f 1 | wc -l)
     CR_COUNT=$(find $FILE -not -type d  -exec file "{}" ";" | grep " CR " | cut -d " " -f 1 | cut -d ":" -f 1 | wc -l)
